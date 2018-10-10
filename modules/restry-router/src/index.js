9f84a68e0b805ea5be34879f64ce382602fce27a
Luke Harris <lukeharris01170@gmail.com>
2018-10-10 12:48:09 +0100

    Fix context not being passed to child block of router

--- modules/restry-router/src/index.js
+++ modules/restry-router/src/index.js
@@ -3,11 +3,11 @@ import matchPath from "./match-path";
 export default routes => context => async (req, res) => {
   for (const route of routes) {
     const { path, block } = route;
-    const match = matchPath(path, req.pathname);
+    const match = matchPath(`${context.path || ""}${path}`, req.pathname);
     if (!match) {
       continue;
     }
-    await block(req, res);
+    await block({ ...context, path: `${context.path || ""}${path}` })(req, res);
     return;
   }
 };
